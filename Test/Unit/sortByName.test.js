a9a03ce3d497c1a6b5c7bbaea078697912d48ff7
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2023-05-05 12:30:24 +0300

    init

---
+++ Test/Unit/sortByName.test.js
@@ -0,0 +1,29 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+
+  it("Equal Books names don`t should be sorted", () => {
+    expect(
+      sorting.sortByName([
+        "Микеланджило",
+        "Микеланджило",
+      ])
+    ).toEqual([
+        "Микеланджило",
+        "Микеланджило",
+    ]);
+  }); 
+});
